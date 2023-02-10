129ae5f5b1f626d3dbdb790388887fb1f06833e5
Kapil Devkota <kdkapildevkota@gmail.com>
2023-02-10 13:53:59 -0500

    added linalg

---
+++ mundo2/linalg.py
@@ -0,0 +1,11 @@
+import scipy as sp
+import numpy as np
+from scipy import linalg
+
+
+
+def compute_k_svd(M, k):
+    U, s, _ = linalg.svd(M)
+    s = np.sqrt(s[:k])
+    U = U[:, :k]
+    return np.matmul(U, s)
